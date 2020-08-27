66d562c5bba663a55bb96b0fb9ded8df4e5ca65d
FERGANY Hazem Ext O-EG/HRCS <lnpx8002@orange.com>
2020-08-27 10:21:36 +0200

    add add-feature component

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeaturesListComponent } from './features-list/features-list.component';
+import { AddFeatureComponent } from './add-feature/add-feature.component';
 
 
 
 @NgModule({
-  declarations: [FeaturesListComponent],
+  declarations: [FeaturesListComponent, AddFeatureComponent],
   imports: [
     CommonModule
   ]
