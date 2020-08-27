ef74d16f37aa35b82e25b50630465b8a56dc8264
FERGANY Hazem Ext O-EG/HRCS <lnpx8002@orange.com>
2020-08-27 15:31:17 +0200

    add customers module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeaturesListComponent } from './features/features-list/features-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeaturesListComponent
   ],
   imports: [
     BrowserModule,
