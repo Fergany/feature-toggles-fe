937de3b6bf287a29d1ed33558e9e13e55d293c25
FERGANY Hazem Ext O-EG/HRCS <lnpx8002@orange.com>
2020-08-27 10:23:49 +0200

    add assign-features component

---
+++ src/app/features/assign-features/assign-features.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assign-features',
+  templateUrl: './assign-features.component.html',
+  styleUrls: ['./assign-features.component.css']
+})
+export class AssignFeaturesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeaturesListComponent } from './features-list/features-list.component';
 import { AddFeatureComponent } from './add-feature/add-feature.component';
+import { AssignFeaturesComponent } from './assign-features/assign-features.component';
 
 
 
 @NgModule({
-  declarations: [FeaturesListComponent, AddFeatureComponent],
+  declarations: [FeaturesListComponent, AddFeatureComponent, AssignFeaturesComponent],
   imports: [
     CommonModule
   ]
